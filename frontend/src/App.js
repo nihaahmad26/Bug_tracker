dacc51f73c65035f05b6758622ac667ab6e1f965
Niha Ahmad <nihaahmad26@gmail.com>
2022-04-22 13:36:45 -0500

    commit on 'login'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import {useSelector} from 'react-redux'
 import './App.css';
 import Login from './Views/Login/login'
 
 function App() {
+  const {auth} = useSelector(state => state)
   return (
    <>
-   <Login />
+   {!auth.LoggedIn ? <Login /> : 
+   <h1>Hello</h1>}
    </>
   );
 }
